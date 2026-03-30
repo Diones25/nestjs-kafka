8f35407f0dd48f3cc5ee3ac2691ec8e3073c7af8
Diones <pereiradiones987@gmail.com>
2026-03-30 16:55:42 -0300

    feat: controller consumer

--- src/consumer/consumer.controller.ts
+++ src/consumer/consumer.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller('consumer')
-export class ConsumerController {}
+export class ConsumerController {
+  @EventPattern('message-topic')
+  handleMessage(@Payload() message: any) {
+    console.log('Received message:', message);
+  }
+}
