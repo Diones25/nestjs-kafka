beb7c7744b7790f287d7b0ed0fc57979111aca6f
Diones <pereiradiones987@gmail.com>
2026-03-30 16:15:42 -0300

    adicionando enpoint para enviar mensagem

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,11 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { MessageService } from './message.service';
 
 @Controller('message')
 export class MessageController {
+
+  constructor(private readonly messageService: MessageService) { }
+
   @EventPattern('test-topic')
   handleMessage(@Payload() data: any) {
     console.log('Mensagem recebida:', data);
   }
 
+  @Get('/send')
+  send() {
+    return this.messageService.sendMessage();
+  }
 }
