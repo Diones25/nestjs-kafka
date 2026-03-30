14951ce74c913fe70304fbc31fe362d9af22b531
Diones <pereiradiones987@gmail.com>
2026-03-30 16:54:05 -0300

    feat: service producer

--- src/producer/producer.service.ts
+++ src/producer/producer.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class ProducerService {}
+export class ProducerService {
+
+  constructor(
+    @Inject('KAFKA_SERVICE') private readonly kafka: ClientKafka,
+  ) { }
+  
+  sendMessage(data: any) {
+    return this.kafka.send('message-topic', data);
+  }
+}
