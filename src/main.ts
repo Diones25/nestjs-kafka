2021900cbec5f1d9737967e464ab96eb0720f3ae
Diones <pereiradiones987@gmail.com>
2026-03-30 17:03:16 -0300

    feat: configurando validação na main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
