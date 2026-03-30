39f429b35cfe4b6c47e257994c0119420bf96d2d
Diones <pereiradiones987@gmail.com>
2026-03-30 16:51:09 -0300

    feat: provider kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ProducerModule } from './producer/producer.module';
 import { ConsumerModule } from './consumer/consumer.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ProducerModule, ConsumerModule],
+  imports: [
+    ProducerModule,
+    ConsumerModule,
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['127.0.0.1:9092'],
+          },
+        },
+      }
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
