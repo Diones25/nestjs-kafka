4b8aeea2b09d0b0aad282403def8b5879e48ad07
Diones <pereiradiones987@gmail.com>
2026-03-30 13:30:53 -0300

    feat: módulo kafka

---
+++ src/kafka/kafka.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'nestjs-app',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'nestjs-consumer-group',
+          }
+        }
+      }
+    ]),
+  ],
+  exports: ['KAFKA_SERVICE']
+})
+export class KafkaModule {}
